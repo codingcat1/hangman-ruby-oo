3575e9c15e9ac5f412156cd81379058f3933e1fd
Catherine Anh Tran & Erica Gordon <student@epicodus.com>
2014-08-07 12:10:04 -0700

    make functionality to initialize a new game with a player name.

--- spec/hangman_game_spec.rb
+++ spec/hangman_game_spec.rb
@@ -2,7 +2,8 @@ require 'rspec'
 require 'game'
 
 describe Game do
-  it "should initialize with a new game" do
-
+  it 'should initialize a new game with a player name' do
+    test_game = Game.new("player_name")
+    expect(test_game).to be_an_instance_of Game
   end
 end
